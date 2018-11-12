828709568558d24d16c31d42c9e73d2c343e3446
Mark Skinner <markskinner850@gmail.com>
2018-11-12 18:30:25 -0500

    Add full screen icons to toolbars
    Functionality not yet added

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Editor = props => (
   <div id='editor_wrap'>
-    <div className='toolbar'>Editor</div>
+    <div className='toolbar'>
+      <div className='title'>Editor</div>
+      <div className='icon'>
+        <i className="icon-fullscreen"></i>
+      </div>
+    </div>
     <textarea id='editor' type='text' onChange={props.updateEditor} value={props.markdownText}></textarea>
   </div>
 );
--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Previewer = props => (
   <div id='preview_wrap'>
-    <div className='toolbar'>Preview</div>
+    <div className='toolbar'>
+      <div className='title'>Preview</div>
+      <div className='icon'>
+        <i className="icon-fullscreen"></i>
+      </div>
+    </div>
     <div id='preview' dangerouslySetInnerHTML={{__html: props.previewResult}}></div>
   </div>
 );
